9c76de66fa9b1a24c6444febb69fddbb5d520732
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-05-25 18:49:35 -0300

    Agrego nuevas migrations y tables, modifico las agregadas anteriormente

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     protected $fillable = [
-        'user_id',
-        'total',
-        'estado',
+        'user_id', 'total', 'estado',
     ];
     
+    // RELACION: Un pedido pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    // RELACION: Un pedido tiene muchos detalles de pedido
     public function detalles()
     {
         return $this->hasMany(DetallePedido::class);
--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Producto extends Model
 {
+    use SoftDeletes; // Activa el borrado logico
+
     protected $fillable = [
         'nombre',
         'descripcion',
@@ -22,9 +25,15 @@ class Producto extends Model
         'activo' => 'boolean',
     ];
 
-    // Un producto pertenece a una sola categoria
+    // RELACION: Un producto pertenece a una sola categoria
     public function categoria()
     {
         return $this->belongsTo(Categoria::class);
+    }
+
+    // RELACION: Un producto puede estan en muchos detalles de pedido
+    public function detalles()
+    {
+        return $this->hasMany(DetallePedido::class);
     }
 }
